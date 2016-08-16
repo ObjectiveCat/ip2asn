762c27bad0502b93e0c740b6b37aaaf071819811
Ivan <ivan8@me.com>
2016-08-16 18:19:07 -0400

    another way to do it

--- iptoasn.py
+++ iptoasn.py
@@ -1,27 +1,25 @@
 import sys
-
-if sys.version_info[0] != 3 or sys.version_info[1] < 5:
-    print("This script requires Python version 3.5")
-    sys.exit(1)
-
+import re
 import ipaddress
 import subprocess
 
+if sys.version_info[0] != 3 or sys.version_info[1] < 5:
+	print("This script requires Python version 3.5")
+	sys.exit(1)
 
 __author__ = "Vadim Petrov"
 __email__ = "vadim@vadimpetrov.com"
 __version__ = "0.1.0"
 __license__ = "GPL"
 
-
 def getasn(ipaddr):
-    ptr = ipaddress.ip_address(ipaddr).reverse_pointer
-    ptr_list = ptr.split('.')[:-2]
-    final_ptr = '.'.join(ptr_list)
-    if ipaddr.version is 4:
-        subprocess.call(['dig', '+short', final_ptr + '.origin.asn.cymru.com', 'TXT'])
-    if ipaddr.version is 6:
-        subprocess.call(['dig', '+short', final_ptr + '.origin6.asn.cymru.com', 'TXT'])
+	ptr = ipaddress.ip_address(ipaddr).reverse_pointer
+	ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', ptr)
+	
+	if ipaddr.version is 4:
+		subprocess.call(['dig', '+short', ip + '.origin.asn.cymru.com', 'TXT'])
+	if ipaddr.version is 6:
+		subprocess.call(['dig', '+short', ip + '.origin6.asn.cymru.com', 'TXT'])
 
 
 getasn(ipaddress.ip_address(sys.argv[1]))
